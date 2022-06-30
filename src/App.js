aa73760393dc4c61f3270d301aad1598c981a304
Ruslan <83825330+CTILET@users.noreply.github.com>
2022-06-30 19:50:16 +0500

    dealers

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Make a car dealers website
         </a>
       </header>
     </div>
